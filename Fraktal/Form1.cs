9d049b0564c84025c1a6447df0c3efc7e960591a
AsuraMTG <eper.karma@gmail.com>
2025-03-10 11:10:43 +0100

    v2

--- Fraktal/Form1.cs
+++ Fraktal/Form1.cs
@@ -17,34 +17,34 @@ namespace Fraktal
             InitializeComponent();
         }
         Color katt;
+        Color szomszedSzine;
         Bitmap b;
         public int X;
         public int Y;
-        /*public void Joe(int X, int Y)
+        public void Joe(Color katt, Color szomszedSzine)
         {
-            katt = b.GetPixel(X, Y);
-            if (katt.R >= (katt.R + Convert.ToInt32(textBox4.Text)) && katt.R <= (katt.R - Convert.ToInt32(textBox4.Text)))
-            {
-
-            }
-        }*/
+            Math.Abs(katt.R - szomszedSzine.R);
+            Math.Abs(katt.G - szomszedSzine.G);
+            Math.Abs(katt.B - szomszedSzine.B);
+        }
         private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
         {
             katt = b.GetPixel(e.X, e.Y);
+            szomszedSzine = b.GetPixel(e.X + 1, e.Y);
             X = e.X;
             Y = e.Y;
             label1.Text = $"RGB: {katt.R}, {katt.G}, {katt.B}";
         }
         private void Form1_Load(object sender, EventArgs e)
-        {
-            b = (Bitmap)Image.FromFile("alap042_000_500_13c.bmp");
+        { 
+            Bitmap b = Properties.Resources.alap042_000_500_13c;
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
             Color color = Color.FromArgb(Convert.ToInt32(textBox1.Text), Convert.ToInt32(textBox2.Text), Convert.ToInt32(textBox3.Text));
             b.SetPixel(X, Y, color);
-
+            Joe(katt, szomszedSzine);
             pictureBox1.Image = b;
         }
     }

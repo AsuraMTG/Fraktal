178b21595ef5b9b4bb1d62e2bd5055b37a8520e0
AsuraMTG <eper.karma@gmail.com>
2025-03-11 11:39:06 +0100

    v3

--- Fraktal/Form1.cs
+++ Fraktal/Form1.cs
@@ -21,22 +21,32 @@ namespace Fraktal
         Bitmap b;
         public int X;
         public int Y;
-        public void Joe(Color katt, Color szomszedSzine)
+        public void Joe(Bitmap image, Point point, Color newColor)
         {
-            Math.Abs(katt.R - szomszedSzine.R);
-            Math.Abs(katt.G - szomszedSzine.G);
-            Math.Abs(katt.B - szomszedSzine.B);
+            int[] xOffsets = { -1, 0, 1, -1, 1, -1, 0, 1 };
+            int[] yOffsets = { -1, -1, -1, 0, 0, 1, 1, 1 };
+
+            for (int i = 0; i < 8; i++)
+            {
+                int newX = point.X + xOffsets[i];
+                int newY = point.Y + yOffsets[i];
+
+                if (newX >= 0 && newX < image.Width && newY >= 0 && newY < image.Height)
+                {
+                    image.SetPixel(newX, newY, newColor);
+                }
+            }
         }
         private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
         {
             katt = b.GetPixel(e.X, e.Y);
-            szomszedSzine = b.GetPixel(e.X + 1, e.Y);
+            szomszedSzine = b.GetPixel(e.X, e.Y);
             X = e.X;
             Y = e.Y;
             label1.Text = $"RGB: {katt.R}, {katt.G}, {katt.B}";
         }
         private void Form1_Load(object sender, EventArgs e)
-        { 
+        {
             Bitmap b = Properties.Resources.alap042_000_500_13c;
         }
 
@@ -44,7 +54,9 @@ namespace Fraktal
         {
             Color color = Color.FromArgb(Convert.ToInt32(textBox1.Text), Convert.ToInt32(textBox2.Text), Convert.ToInt32(textBox3.Text));
             b.SetPixel(X, Y, color);
-            Joe(katt, szomszedSzine);
+
+            Point pose = new Point(X, Y);
+            Joe(b, pose, color);
             pictureBox1.Image = b;
         }
     }
